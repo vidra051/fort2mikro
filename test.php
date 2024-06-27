7dfc1a39d6ba8fdced2af3ad9c11248f69d39f59
Vedran Blažević <140592739+vidra051@users.noreply.github.com>
2024-06-27 09:42:04 +0200

    test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 include_once('forti2mikrotik.php');
 
 $mikro = new forti2mikro();
-echo $mikro->convert('C:\Users\Mick\Documents\Fortinet\FGT50E5618029408-Rijeka_20230613_1159.conf');
+echo $mikro->convert('FGT50E5618029408_20230613_1159.conf');
 
 
 
